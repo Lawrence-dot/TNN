c97596f86949005ca870a7709eee9ed06e58ceb0
Lawrence-dot <damilareojediran3@gmail.com>
2020-11-08 20:12:58 -0800

    update

--- js/index.js
+++ js/index.js
@@ -2,14 +2,20 @@ var toggler = document.getElementById("toggler");
 let sidebar = document.getElementsByClassName("side-bar");
 let theForm = document.getElementById("top-form");
 let cursor = document.getElementById("cursor");
-let pageContent = document.getElementById("");
+let sideCol = document.getElementById("side-col");
+let pageContent = document.getElementById("page-col");
 
 toggler.onclick = function name() {
     theForm.classList.toggle("hide");
+    sideCol.classList.toggle("d-block");
     cursor.classList.toggle("close");
     if (cursor.classList.contains("close")) {
         cursor.innerHTML = "&times;";
     } else {
         cursor.innerHTML = "&#8801;Menu";
     };
+
+    pageContent.classList.toggle("hhh");
+
+    // pageContent.style.paddingLeft = "250px";
 };
